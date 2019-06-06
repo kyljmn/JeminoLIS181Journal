846917710dd4c6c868b45446c7916279d025ad95
kyljmn <kylejemino@gmail.com>
2019-06-07 06:47:19 +0800

    basic app.js added views directory and index page

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser");
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("index");
+})
+
+app.listen(8080, function(){
+  console.log("Server Running");
+});
