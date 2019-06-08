dd0ed99a0a60fbec598a99d0c527cb7d40fb6055
kyljmn <kylejemino@gmail.com>
2019-06-09 05:18:30 +0800

    basic CRUD functionality for blog posts

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
+    methodOverride = require("method-override"),
     mongoose = require("mongoose");
 
 mongoose.set('useNewUrlParser', true);
 mongoose.connect("mongodb+srv://kyljmn:123@kyljmn-mvoiy.mongodb.net/test?retryWrites=true&w=majority");
+
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
 
 var   blogSchema =  new mongoose.Schema({
   title: String,
@@ -18,6 +22,10 @@ var   blogSchema =  new mongoose.Schema({
 var Blog = mongoose.model("Blog", blogSchema);
 
 app.get("/", function(req, res){
+  res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
   Blog.find({}, function(err, blogs){
     if(err){
       res.send(err);
@@ -40,6 +48,47 @@ app.post("/blogs", function(req,res){
     }
   });
 });
+
+app.get("/blogs/:id", function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if(err){
+      res.send(err);
+    } else {
+      res.render("show", {blog: foundBlog});
+    }
+  });
+});
+
+app.get("/blogs/:id/edit", function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if(err){
+      res.send(err);
+    } else {
+      res.render("edit", {foundBlog : foundBlog});
+    }
+  });
+})
+
+app.put("/blogs/:id", function(req, res){
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedCampground){
+    if(err){
+      res.send(err);
+    } else{
+      res.redirect("/blogs/" + updatedCampground._id);
+    }
+  });
+});
+
+app.delete("/blogs/:id", function(req, res){
+  Blog.findByIdAndRemove(req.params.id, function(err){
+    if(err){
+      res.send(err);
+    } else {
+      res.redirect("/blogs");
+    }
+  })
+})
+
 
 app.listen(8080, function(){
   console.log("Server Running");
